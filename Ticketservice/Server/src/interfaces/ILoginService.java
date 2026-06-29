d3eb34d950136a85e1fca29ed2d71e9ec0b46449
VlMay <mayervalentin101@gmail.com>
2026-06-29 13:34:31 +0200

    Delete Ticketservice directory

--- Ticketservice/Server/src/interfaces/ILoginService.java
+++
@@ -1,9 +0,0 @@
-package Ticketservice.Server.src.interfaces;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ILoginService extends Remote {
-    boolean authentifiziereKunde(int id, String name) throws RemoteException;
-    boolean authentifiziereMitarbeiter(int id, String name) throws RemoteException;
-}
